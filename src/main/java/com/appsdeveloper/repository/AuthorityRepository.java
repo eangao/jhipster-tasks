b6581f7b335a5df658b630d61a207e85e8dd6559
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-05-31 11:11:22 +0800

    Jhipster initial setup

---
+++ src/main/java/com/appsdeveloper/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.appsdeveloper.repository;
+
+import com.appsdeveloper.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
